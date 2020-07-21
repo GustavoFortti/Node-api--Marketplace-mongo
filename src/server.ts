092c823145db76ea269d5b31d6bdfaf7e1b0fc8a
gustavofortti <gustavofortti@gmail.com>
2020-07-21 20:18:47 -0300

    Arquivo rotas implementado

--- src/server.ts
+++ src/server.ts
@@ -1,41 +1,10 @@
-
 import express, { response, request } from 'express'; 
+import routes from './routes';
 
 const app = express();
 app.use(express.json());
 
-const users = ([
-    'Gustavo',
-    'Carlos',
-    'Diego',
-    'Robson'
-]);
-
-app.get('/users', (request, response) => {
-    const search = String(request.query.search);
-
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-
-    return response.json(filteredUsers);
-});
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const user = users[id];
-
-    return response.json(user);
-});
-
-app.post('/users', (request, response) => {
-    const data = request.body;
-
-    const user = {
-        nome: data.name,
-        email: data.email
-    }
-
-    return response.json(user);
-})
+app.use(routes);
 
 app.listen(3333); 
 
